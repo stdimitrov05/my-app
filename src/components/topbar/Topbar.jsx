8a07bd5db6d9deec9f339ed8016dbb94961612bc
stdimitrov05 <stdimitrov8b@gmail.com>
2021-06-06 09:24:32 +0300

    update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -11,7 +11,7 @@ export default function Topbar() {
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">InstaScript Social Media</span>
+          <span className="logo">Lamasocial</span>
         </Link>
       </div>
       <div className="topbarCenter">
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -21,9 +21,9 @@ export default function Login() {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">IstaScirpt Social Media</h3>
+          <h3 className="loginLogo">Lamasocial</h3>
           <span className="loginDesc">
-            Connect with friends and the world around you on InstaScript Social Media.
+            Connect with friends and the world around you on Lamasocial.
           </span>
         </div>
         <div className="loginRight">
